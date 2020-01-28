24e567bc3918e2e9607bce66cc840485c4a4f17b
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-01-28 10:41:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,52 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
+<style>
 
-<h2>Using the XMLHttpRequest Object</h2>
-
-<div id="demo">
-</div>
-
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
+    .content{
+        display: flex;
+        justify-content: center;
     }
-  };
-  xhttp.open("GET", "emails.xml", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+    .wrap{
+        display: flex;
+        flex-direction: column;
+        border: 1px solid black;
+        width: 30rem;
+    }
+    .border{
+        border: 1px solid black;
+        max-width: 40rem;
+    }
+</style>
+
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://new-apptet.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("pet");
+?>
+ <div class="content">
+ <h1>Pets</h1>
+ </div>
+ <div class="content">
+
+ <div class="wrap">
+ <?php
+ foreach( $content as $data )
+ {?>
+     <div class="border">
+     <?php
+     $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+     $type = $data->getElementsByTagName("type")->item(0)->nodeValue;
+     $age = $data->getElementsByTagName("age")->item(0)->nodeValue;
+     $color = $data->getElementsByTagName("color")->item(0)->nodeValue;
+     echo "<ul><h2>$name - $type</h2>
+              <ul>
+                  <li>Age: $age y/o</li>
+                  <li>Color: $color color</li>
+              </ul>
+          </ul>";
+    ?>
+     </div>
+  <?php
+ }
+?>
+</div>
+</div>
